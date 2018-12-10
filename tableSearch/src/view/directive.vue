2b124f839c9ac4459c30e5b365d7a74c8eb26b1e
wyyyyyy <301059120@qq.com>
2018-12-10 21:58:57 +0800

    2018-12-10

--- tableSearch/src/view/directive.vue
+++ tableSearch/src/view/directive.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <input v-focus type="text" placeholder="请输入">
-    <div :class="c" class="demo1" v-if="isShow">
+    <div :class="c" class="demo" v-if="isShow">
       <span v-for="item in sz">{{item}}</span>
     </div>
   </div>
@@ -9,6 +9,27 @@
 
 <script>
 var html = '<div :class="c" class="demo" v-if="isShow"><span v-for="item in sz">{{item}}</span></div>'
+
+
+const ncname = '[a-zA-Z][\\w\\-\\.]*';
+const singleAttrIdentifier = /([^\s"'<>/=]+)/;
+const singleAttrAssign = /(?:=)/;
+const singleAttrValues = [
+  /"([^"]*)"+/.source,
+  /"([^']*)'+/.source,
+  /"([^\s"'=<>`]+)/.source,
+]
+const attribute = new RegExp(
+  '^\\s*' + singleAttrIdentifier.source +
+  '(?:\\s*(' + singleAttrAssign.source + ')' +
+  '\\s*(?:' + singleAttrValues.join('|') + '))?'
+)
+const qnameCapture = '((?:' + ncname + '\\:)?' + ncname + ')'
+const startTagOpen = new RegExp('<' + qnameCapture)
+const startTagClose = /^\s*(\/?)>/
+const endTag = new RegExp('^<\\/' + qnameCapture + '[^>]*>')
+const defaultTagRE = /\{\{((?:.|\n)+?)\}\}/g
+const forAliasRE = /(.*?)\s+(?:in|of)\s+(.*)/
 export default {
   name: 'Directives',
   directives: {
