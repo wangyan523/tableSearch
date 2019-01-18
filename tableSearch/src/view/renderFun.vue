3c41e7b8e5fb23182ca06c8b6933b5ecae7b5cdf
wyyyyyy <301059120@qq.com>
2019-01-18 21:46:46 +0800

    2019-1-18

--- tableSearch/src/view/renderFun.vue
+++ tableSearch/src/view/renderFun.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
-    renderfun page
-  </div>
+  
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        show: false
+      }
+    },
+    render: (h) => {
+      let childNode;
+      if (this.show) {
+        childNode = h('p','内容1')
+      } else {
+        childNode = h('p', '内容2')
+      }
+      return h('div', {
+        attrs : {
+          id: 'main'
+        },
+        class: {
+          container: true
+        },
+        style: {
+          color: 'red'
+        }
+      }, [childNode])
+    }
   }
 </script>
 
